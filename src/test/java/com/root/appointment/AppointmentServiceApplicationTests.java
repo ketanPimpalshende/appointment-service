a083952ca69faca7659f5c17a7ee3f832b3e5955
Ketan Pimpalshende <ketanpimpalshende@gmail.com>
2024-06-09 13:40:34 +0530

    initial setup

---
+++ src/test/java/com/root/appointment/AppointmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.root.appointment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
